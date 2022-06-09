2252c3bd3d929071fd43a37a624ef02705cdf4fd
MariaDiezEsteve <diezesteve@outlook.es>
2022-06-09 16:50:09 +0200

    cambio fotos en el carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,34 +5,34 @@ import Carousel from 'react-bootstrap/Carousel'
 function Home() {
   return (
     <div>
-
+      <br />
       <h1 className='titleHome'>Wellcome to the React Hotel</h1>
       <Carousel className="carousel" fade="true">
         <Carousel.Item >
           <img
             className="d-block mx-auto"
-            src="https://res.cloudinary.com/dm5zetu40/image/upload/v1654716438/Carrousel%20de%20hoteles/download_uvphyn.jpg"
+            src="https://res.cloudinary.com/dm5zetu40/image/upload/v1654764500/Imagenes%20hoteles/hoteles-originales-13_omdjki.jpg"
             alt="First slide"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block mx-auto"
-            src="https://res.cloudinary.com/dm5zetu40/image/upload/v1654716441/Carrousel%20de%20hoteles/download_ifrszj.jpg"
+            src="https://res.cloudinary.com/dm5zetu40/image/upload/v1654764501/Imagenes%20hoteles/rural_il4tqc.jpg"
             alt="Second slide"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block mx-auto"
-            src="https://res.cloudinary.com/dm5zetu40/image/upload/v1654716453/Carrousel%20de%20hoteles/images_rywcnx.jpg"
+            src="https://res.cloudinary.com/dm5zetu40/image/upload/v1654764500/Imagenes%20hoteles/578453891_184024305_1706x960_sepp2k.webp"
             alt="Third slide"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block mx-auto"
-            src="https://res.cloudinary.com/dm5zetu40/image/upload/v1654716496/Carrousel%20de%20hoteles/images_latbsr.jpg"
+            src="https://res.cloudinary.com/dm5zetu40/image/upload/v1654764500/Imagenes%20hoteles/hoteles-originales-11_cyqrwt.jpg"
             alt="Fourt slide"
           />
         </Carousel.Item>
